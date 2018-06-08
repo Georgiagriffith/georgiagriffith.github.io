ca915c836ae464d5cc7a7ef77f67d1d5ab91d490
Georgiagriffith <37203979+Georgiagriffith@users.noreply.github.com>
2018-06-08 00:13:37 -0700

    Revert "New additions"

--- gg_htmlboilerplate_version2/assets/js/main.js
+++ gg_htmlboilerplate_version2/assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-// Add jQuery here..not sure what this is//
+// Add jQuery here
 $('.menu').click(function(){
 $('.nav').addClass('active');
 
@@ -9,50 +9,19 @@ $('.nav').addClass('active');
 
 
 
-// Hide show attempt//
 
 
 
 
-$(document).ready(function(){
-    $(".hide").click(function(){
-        $(".abstracta").hide();
-    });
-    $(".show").click(function(){
-        $(".abstracta").show();
-    });
-});
 
 
 
-$(document).ready(function(){
-    $(".hide1").click(function(){
-        $(".animals").hide();
-    });
-    $(".show1").click(function(){
-        $(".animals").show();
-    });
-});
 
 
-$(document).ready(function(){
-    $(".hide2").click(function(){
-        $(".people").hide();
-    });
-    $(".show2").click(function(){
-        $(".people").show();
-    });
-});
 
 
-$(document).ready(function(){
-    $(".hide3").click(function(){
-        $(".objects").hide();
-    });
-    $(".show3").click(function(){
-        $(".objects").show();
-    });
-});
+
+
 
 
 // dont remove this line
